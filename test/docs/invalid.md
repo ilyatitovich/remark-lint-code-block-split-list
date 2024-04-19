789a9d75ea333ef55b16ffbfdc7eb037aef3e08d
ilyatitovich <ilyatitovich@gmail.com>
2024-04-19 16:14:47 +0200

    Add tests

---
+++ test/docs/invalid.md
@@ -0,0 +1,21 @@
+# My List with Code Block
+
+1. Item 1
+
+```sh
+pnpm run dev
+```
+
+2. Item 2
+   * Sub Item 1
+
+    ```sh
+    pnpm run dev
+    ```
+
+   * Sub Item 2
+3. Item 3
+
+   ```sh
+   pnpm run dev
+   ```
---
+++ test/test.js
@@ -0,0 +1,37 @@
+import fs from 'fs'
+import path from 'path'
+import assert from 'node:assert/strict'
+import test from 'node:test'
+import { remark } from 'remark'
+import { compareMessage } from 'vfile-sort'
+import rremarkLintCodeBlockSplitList from '../index.js'
+
+const invalidMdPath = path.join(import.meta.dirname, 'docs', 'invalid.md')
+const validMdPath = path.join(import.meta.dirname, 'docs', 'valid.md')
+
+const invalidMd = fs.readFileSync(invalidMdPath, 'utf-8')
+const validMd = fs.readFileSync(validMdPath, 'utf-8')
+
+test('code block split list', async () => {
+  const result = await remark()
+    .use(rremarkLintCodeBlockSplitList)
+    .process(invalidMd)
+
+  result.messages.sort(compareMessage)
+
+  assert.deepEqual(
+    result.messages.map(d => d.reason),
+    [
+      'Add 3 spaces to the beginning of the code block to align with the list',
+      'Add 1 space to the beginning of the code block to align with the list'
+    ]
+  )
+})
+
+test('no errors found', async () => {
+  const result = await remark()
+    .use(rremarkLintCodeBlockSplitList)
+    .process(validMd)
+
+  assert.strictEqual(result.messages.length, 0)
+})
